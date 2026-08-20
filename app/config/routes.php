ab9abf854075aee5f22bf53d3347fd7c03e320d3
Neo <magnayeneo@gmail.com>
2026-08-20 19:08:13 +0800

    get fixed 2

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,13 +3,9 @@
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get(
-<<<<<<< HEAD
-    '/views/student',
-=======
-    '/views/student/',
->>>>>>> 0b527345fda277eb95a866c60db8bfd4bf6dd71d
+    '/views/student/index',
     'StudentController::index'
-); 
+);
 
 $router->get(
     '/views/student/profile',
